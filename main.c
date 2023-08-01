96b6d8e877b23665147ee30c3a90c1e4159f4d7a
GiuliaBortone <giulia.bortone@mail.polimi.it>
2023-08-01 19:40:31 +0200

    implement find path forward algorithm

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 //#include <stdbool.h>
 
 #define MAX_VEHICLES 512
@@ -16,9 +17,15 @@ typedef struct {
 } Station; // Node
 
 typedef struct {
+    int distance;
+    int distancePlusMax;
+} StationForPath;
+
+typedef struct {
     Station *stations;
     int num_stations;
 } Highway; //Graph
+
 
 // region initialization and deletion of graph
 void initializeHighway(Highway *highway) {
@@ -146,11 +153,11 @@ void removeVehicle(Highway *highway, int distance, int vehicle) {
                     highway->stations[i].cars->num_vehicles--;
                     maxHeapify(highway->stations[i].cars, j);
                     printf("rottamata\n");
-
                     return;
                 }
             }
             printf("non rottamata\n"); // vehicle not found
+            return;
         }
     }
 
@@ -171,21 +178,8 @@ void addStation(Highway *highway, int distance, int num_vehicles, int *cars) {
     // add the new station
     highway->num_stations++;
     highway->stations = (Station *) realloc(highway->stations, highway->num_stations * sizeof(Station));
-
-    // Find the correct position to insert the new station based on distance
-    int insert_index = 0;
-    while (insert_index < highway->num_stations - 1 && highway->stations[insert_index].distance < distance) {
-        insert_index++;
-    }
-
-    // Move the existing stations to make room for the new one
-    for (int i = highway->num_stations - 1; i > insert_index; i--) {
-        highway->stations[i] = highway->stations[i - 1];
-    }
-
-    // Insert the new station at the correct position
-    highway->stations[insert_index].distance = distance;
-    highway->stations[insert_index].cars = createHeap(num_vehicles, cars);
+    highway->stations[highway->num_stations - 1].distance = distance;
+    highway->stations[highway->num_stations - 1].cars = createHeap(num_vehicles, cars);
 
     printf("aggiunta\n");
 }
@@ -209,8 +203,149 @@ void removeStation(Highway *highway, int distance) {
 
     printf("non demolita\n");
 }
-
 // endregion graph methods
+
+// region find path methods
+void swap(Station *s1, Station *s2) {
+    Station temp = *s1;
+    *s1 = *s2;
+    *s2 = temp;
+}
+
+void heapify(Highway *highway, int dim, int index) {
+    int largest = index;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+
+    if (left < dim && highway->stations[left].distance > highway->stations[largest].distance) {
+        largest = left;
+    }
+
+    if (right < dim && highway->stations[right].distance > highway->stations[largest].distance) {
+        largest = right;
+    }
+
+    if (largest != index) {
+        swap(&highway->stations[index], &highway->stations[largest]);
+        heapify(highway, dim, largest);
+    }
+
+}
+
+void orderStationsHeapSort(Highway *highway) {
+    int dim = highway->num_stations;
+
+    for (int i = dim / 2 - 1; i >= 0; i--) {
+        heapify(highway, dim, i);
+    }
+
+    for (int i = dim - 1; i > 0; i--) {
+        swap(&highway->stations[0], &highway->stations[i]);
+        heapify(highway, i, 0);
+    }
+
+}
+
+bool isPresent(StationForPath *path, int dim, int distance) {
+    for (int i = 0; i < dim; i++) {
+        if (path[i].distance == distance)
+            return true;
+    }
+    return false;
+}
+
+void findShortestPath(Highway *highway, int start, int target) {
+    Station starting_station;
+    starting_station.distance = 0;
+    starting_station.cars = NULL;
+
+    int start_index = -1, target_index = -1;
+
+    // Search for start and target stations
+    for (int i = 0; i < highway->num_stations && (start_index == -1 || target_index == -1); i++) {
+        if (highway->stations[i].distance == start) {
+            starting_station = highway->stations[i];
+            start_index = i;
+        } else if (highway->stations[i].distance == target) {
+            target_index = i;
+        }
+    }
+
+    // Check if stations where found
+    if (start_index == -1 || target_index == -1) {
+        printf("nessun percorso\n"); // start and target stations not found
+        return;
+    }
+
+    // Initialize path
+    StationForPath *path = (StationForPath *) malloc(sizeof(StationForPath) * (target_index - start_index + 1));
+    int path_dim = 1;
+
+    path[0].distance = start;
+    path[0].distancePlusMax = start + starting_station.cars->autonomy[0];
+
+
+    int last_no = start_index + 1;
+    int old_last = last_no;
+    int found_target = 0;
+    for (int i = start_index; i < target_index + 1 && !found_target; i++) {
+        int reach = highway->stations[i].distance + highway->stations[i].cars->autonomy[0];
+
+        for (int j = last_no; j < target_index + 1 && !found_target; j++) {
+            int current_distance = highway->stations[j].distance;
+
+            if (reach >= current_distance) {
+                if (!isPresent(path, path_dim, current_distance)) {
+                    int max = current_distance + highway->stations[j].cars->autonomy[0];
+
+                    path[path_dim].distance = current_distance;
+                    path[path_dim].distancePlusMax = max;
+                    path_dim++;
+                }
+
+                if (highway->stations[j].distance == target) {
+                    found_target = 1;
+                }
+            } else {
+                last_no = j;
+                break;
+            }
+        }
+
+        if (old_last == last_no) {
+            break;
+        }
+    }
+
+    if (found_target == 0) {
+        printf("nessun percorso\n"); // couldn't reach the target station
+        return;
+    }
+
+    int last_distance = path[path_dim - 1].distance;
+    for (int i = path_dim - 2; i > 0; i--) {
+        if (path[i - 1].distancePlusMax >= last_distance) {
+            path[i].distance = -1;
+        } else {
+            last_distance = path[i].distance;
+        }
+    }
+
+    for (int i = 0; i < path_dim - 1; i++) {
+        if (path[i].distance != -1) {
+            printf("%d ", path[i].distance);
+        }
+    }
+    printf("%d\n", path[path_dim - 1].distance);
+
+    free(path);
+}
+
+void findShortestPathReverse(Highway *highway, int start, int target) {
+    printf("nessun percorso\n");
+}
+
+// endregion find paths methods
 
 // region print
 void printHeap(Vehicles *v) {
@@ -235,7 +370,7 @@ int main() {
     initializeHighway(&highway);
 
     char command[20];
-    int distance, num_vehicles, autonomy; //start_station, target_station;
+    int distance, num_vehicles, autonomy, start_station, target_station;
     int vehicles[MAX_VEHICLES];
 
     while (scanf("%s", command) != EOF) {
@@ -265,15 +400,20 @@ int main() {
             }
             removeVehicle(&highway, distance, autonomy);
         } else if (strcmp(command, "pianifica-percorso") == 0) {
-            //    if (scanf("%d %d", &start_station, &target_station) != 2) {
-            //        return 1;
-            //    }
-            //    findShortestPath(&graph, start_station, target_station);
-            printf("nessun percorso\n");
+            if (scanf("%d %d", &start_station, &target_station) != 2) {
+                return 1;
+            }
+
+            orderStationsHeapSort(&highway);
+
+            if (start_station < target_station) {
+                findShortestPath(&highway, start_station, target_station);
+            } else if (start_station > target_station) {
+                findShortestPathReverse(&highway, start_station, target_station);
+            } else
+                printf("%d\n", start_station);
         }
     }
-
-    printGraph(&highway);
 
     freeHighway(&highway);
 
