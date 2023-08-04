ee80f279f30db501a0d2eced0b7fcfc267fb9ad9
GiuliaBortone <giulia.bortone@mail.polimi.it>
2023-08-04 12:24:33 +0200

    last changes

--- main.c
+++ main.c
@@ -120,138 +120,156 @@ Vehicles *createHeap(int num_vehicles, int *cars) {
 }
 
 void addVehicle(Highway *highway, int distance, int vehicle) {
-    for (int i = highway->num_stations - 1; i >= 0; i--) {
-        if (highway->stations[i].distance == distance) {
+    int low = 0, high = highway->num_stations - 1;
+    int index = -1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
 
-            int num_vehicles = highway->stations[i].cars->num_vehicles;
-
-            if (num_vehicles < MAX_VEHICLES) {
-                highway->stations[i].cars->autonomy[num_vehicles] = vehicle;
-                insertionHelper(highway->stations[i].cars, highway->stations[i].cars->num_vehicles);
-                highway->stations[i].cars->num_vehicles++;
-
-                printf("aggiunta\n");
-                return;
-            }
-            printf("non aggiunta\n"); // max vehicles reached
-            return;
+        if (highway->stations[mid].distance == distance) {
+            index = mid;
+            break;
+        } else if (highway->stations[mid].distance < distance) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
         }
     }
 
-    printf("non aggiunta\n"); // station not found
+    if (index == -1) {
+        printf("non aggiunta\n"); // station not found
+        return;
+    }
+
+    int num_vehicles = highway->stations[index].cars->num_vehicles;
+
+    if (num_vehicles < MAX_VEHICLES) {
+        highway->stations[index].cars->autonomy[num_vehicles] = vehicle;
+        insertionHelper(highway->stations[index].cars, highway->stations[index].cars->num_vehicles);
+        highway->stations[index].cars->num_vehicles++;
+
+        printf("aggiunta\n");
+        return;
+    }
+    printf("non aggiunta\n"); // max vehicles reached
 }
 
 void removeVehicle(Highway *highway, int distance, int vehicle) {
-    for (int i = 0; i < highway->num_stations; i++) {
-        if (highway->stations[i].distance == distance) {
-            int num_vehicles = highway->stations[i].cars->num_vehicles;
+    int low = 0, high = highway->num_stations - 1;
+    int index = -1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
 
-            if (num_vehicles == 0) {
-                printf("non rottamata\n");
-                return;
-            }
-
-            for (int j = 0; j < num_vehicles; j++) {
-                if (highway->stations[i].cars->autonomy[j] == vehicle) {
-                    highway->stations[i].cars->autonomy[j] = highway->stations[i].cars->autonomy[num_vehicles - 1];
-                    highway->stations[i].cars->num_vehicles--;
-                    if (highway->stations[i].cars->num_vehicles == 0) {
-                        highway->stations[i].cars->autonomy[0] = 0;
-                    } else
-                        maxHeapify(highway->stations[i].cars, j);
-                    printf("rottamata\n");
-                    return;
-                }
-            }
-            printf("non rottamata\n"); // vehicle not found
-            return;
+        if (highway->stations[mid].distance == distance) {
+            index = mid;
+            break;
+        } else if (highway->stations[mid].distance < distance) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
         }
     }
 
-    printf("non rottamata\n"); // station not found
+    if (index == -1) {
+        printf("non rottamata\n"); // station not found
+        return;
+    }
+
+    int num_vehicles = highway->stations[index].cars->num_vehicles;
+
+    if (num_vehicles == 0) {
+        printf("non rottamata\n");
+        return;
+    }
+
+    for (int j = 0; j < num_vehicles; j++) {
+        if (highway->stations[index].cars->autonomy[j] == vehicle) {
+            highway->stations[index].cars->autonomy[j] = highway->stations[index].cars->autonomy[num_vehicles - 1];
+            highway->stations[index].cars->num_vehicles--;
+            if (highway->stations[index].cars->num_vehicles == 0) {
+                highway->stations[index].cars->autonomy[0] = 0;
+            } else
+                maxHeapify(highway->stations[index].cars, j);
+            printf("rottamata\n");
+            return;
+        }
+    }
+    printf("non rottamata\n"); // vehicle not found
 }
 // endregion max heap methods for vehicles in each station
 
 // region graph methods
 void addStation(Highway *highway, int distance, int num_vehicles, int *cars) {
+    int last_min_index = highway->num_stations - 1;
     // check if a station already exists at the given distance
     for (int i = 0; i < highway->num_stations; i++) {
         if (highway->stations[i].distance == distance) {
             printf("non aggiunta\n");
             return;
         }
-    }
 
-    // add the new station
-    highway->num_stations++;
-    highway->stations = (Station *) realloc(highway->stations, highway->num_stations * sizeof(Station));
-    highway->stations[highway->num_stations - 1].distance = distance;
-    highway->stations[highway->num_stations - 1].cars = createHeap(num_vehicles, cars);
-
-    printf("aggiunta\n");
-}
-
-void removeStation(Highway *highway, int distance) {
-    for (int i = 0; i < highway->num_stations; i++) {
-        if (highway->stations[i].distance == distance) {
-            free(highway->stations[i].cars->autonomy);
-
-            for (int j = i; j < highway->num_stations - 1; j++) {
-                highway->stations[j] = highway->stations[j + 1];
-            }
-
-            highway->num_stations--;
-            highway->stations = (Station *) realloc(highway->stations, highway->num_stations * sizeof(Station));
-
-            printf("demolita\n");
-            return;
+        if (distance < highway->stations[i].distance) {
+            last_min_index = i - 1;
+            break;
         }
     }
 
-    printf("non demolita\n");
+    highway->num_stations++;
+    highway->stations = (Station *) realloc(highway->stations, highway->num_stations * sizeof(Station));
+    if (last_min_index == highway->num_stations - 2) {
+        highway->stations[highway->num_stations - 1].distance = distance;
+        highway->stations[highway->num_stations - 1].cars = createHeap(num_vehicles, cars);
+        printf("aggiunta\n");
+        return;
+    }
+
+    for (int i = highway->num_stations - 1; i >= 0; i--) {
+        if (i - 1 != last_min_index) {
+            highway->stations[i].distance = highway->stations[i - 1].distance;
+            highway->stations[i].cars = highway->stations[i - 1].cars;
+        } else {
+            highway->stations[i].distance = distance;
+            highway->stations[i].cars = createHeap(num_vehicles, cars);
+            printf("aggiunta\n");
+            return;
+        }
+    }
+}
+
+void removeStation(Highway *highway, int distance) {
+    int low = 0, high = highway->num_stations - 1;
+    int index = -1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (highway->stations[mid].distance == distance) {
+            index = mid;
+            break;
+        } else if (highway->stations[mid].distance < distance) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    if (index == -1) {
+        printf("non demolita\n"); // station not found
+        return;
+    }
+
+    free(highway->stations[index].cars->autonomy);
+
+    for (int j = index; j < highway->num_stations - 1; j++) {
+        highway->stations[j] = highway->stations[j + 1];
+    }
+
+    highway->num_stations--;
+    highway->stations = (Station *) realloc(highway->stations, highway->num_stations * sizeof(Station));
+
+    printf("demolita\n");
 }
 // endregion graph methods
 
 // region find path methods
-void swap(Station *s1, Station *s2) {
-    Station temp = *s1;
-    *s1 = *s2;
-    *s2 = temp;
-}
-
-void heapify(Highway *highway, int dim, int index) {
-    int largest = index;
-    int left = 2 * index + 1;
-    int right = 2 * index + 2;
-
-    if (left < dim && highway->stations[left].distance > highway->stations[largest].distance) {
-        largest = left;
-    }
-
-    if (right < dim && highway->stations[right].distance > highway->stations[largest].distance) {
-        largest = right;
-    }
-
-    if (largest != index) {
-        swap(&highway->stations[index], &highway->stations[largest]);
-        heapify(highway, dim, largest);
-    }
-
-}
-
-void orderStationsHeapSort(Highway *highway) {
-    int dim = highway->num_stations;
-
-    for (int i = dim / 2 - 1; i >= 0; i--) {
-        heapify(highway, dim, i);
-    }
-
-    for (int i = dim - 1; i > 0; i--) {
-        swap(&highway->stations[0], &highway->stations[i]);
-        heapify(highway, i, 0);
-    }
-}
-
 bool isPresent(StationForPath path[], int dim, int distance) {
     for (int i = 0; i < dim; i++) {
         if (path[i].distance == distance)
@@ -261,34 +279,27 @@ bool isPresent(StationForPath path[], int dim, int distance) {
 }
 
 void findShortestPathForward(Highway *highway, int start, int target) {
-    Station starting_station;
-    starting_station.distance = 0;
-    starting_station.cars = NULL;
+    int low = 0, high = highway->num_stations - 1;
+    int start_index = -1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
 
-    int start_index = -1, target_index = -1;
-
-    // Search for start and target stations
-    for (int i = 0; i < highway->num_stations && (start_index == -1 || target_index == -1); i++) {
-        if (highway->stations[i].distance == start) {
-            starting_station = highway->stations[i];
-            start_index = i;
-        } else if (highway->stations[i].distance == target) {
-            target_index = i;
+        if (highway->stations[mid].distance == start) {
+            start_index = mid;
+            break;
+        } else if (highway->stations[mid].distance < start) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
         }
     }
 
-    // Check if stations where found
-    if (start_index == -1 || target_index == -1) {
-        printf("nessun percorso\n"); // start and target stations not found
-        return;
-    }
-
     // Initialize path
-    StationForPath path[target_index - start_index + 1];
+    StationForPath path[highway->num_stations - start_index + 1];
     int path_dim = 1;
 
     path[0].distance = start;
-    path[0].reach = start + starting_station.cars->autonomy[0];
+    path[0].reach = start + highway->stations[start_index].cars->autonomy[0];
     path[0].parent = -1;
     path[0].costToReach = 0;
     path[0].visited = false;
@@ -296,7 +307,7 @@ void findShortestPathForward(Highway *highway, int start, int target) {
     int last_no = start_index + 1;
     int old_last = last_no;
     int found_target = 0;
-    for (int i = start_index; i < target_index + 1 && !found_target; i++) {
+    for (int i = start_index; i < highway->num_stations && !found_target; i++) {
         int reach = highway->stations[i].distance + highway->stations[i].cars->autonomy[0];
 
         if (!isPresent(path, path_dim, highway->stations[i].distance)) {
@@ -304,7 +315,7 @@ void findShortestPathForward(Highway *highway, int start, int target) {
             return;
         }
 
-        for (int j = last_no; j < target_index + 1 && !found_target; j++) {
+        for (int j = last_no; j < highway->num_stations && !found_target; j++) {
             int current_distance = highway->stations[j].distance;
 
             if (reach >= current_distance) {
@@ -526,13 +537,12 @@ int main() {
             if (scanf("%d %d", &distance, &autonomy) != 2) {
                 return 1;
             }
+
             removeVehicle(&highway, distance, autonomy);
         } else if (strcmp(command, "pianifica-percorso") == 0) {
             if (scanf("%d %d", &start_station, &target_station) != 2) {
                 return 1;
             }
-
-            orderStationsHeapSort(&highway);
 
             if (start_station < target_station) {
                 findShortestPathForward(&highway, start_station, target_station);
